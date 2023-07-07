5d26cd663a990b9a11baaa0f3cd8e118ce9790a7
musnow <ezplayingd@126.com>
2023-07-07 14:57:00 +0800

    feat(main): 新增本地token有效期的判断

--- main.py
+++ main.py
@@ -7,11 +7,29 @@ from utils.bdyUpd import BaiDuWangPan
 from utils.confLoad import Config,write_config_file
 from utils import gtime
 
+def is_need_auth():
+    """通过config判断是否需要重新授权"""
+    if "BDY_USER_TOKEN_OUTDATE" in Config:
+        cur_time = time.time()
+        if Config["BDY_USER_TOKEN_OUTDATE"] > cur_time:
+            time_diff = Config["BDY_USER_TOKEN_OUTDATE"] - cur_time 
+            if time_diff <= 3*3600*24: # 小于3天，就需要重新获取token
+                return True
+            else:
+                return False
+        else:# 当前时间都比过期时间大了，直接退出
+            return True
+    else:# 不存在键值，直接退出
+        return True
 
 def auth_bdy():
     """先进行百度云验证，需要等待用户输入验证码"""
     try:
-        bdy = BaiDuWangPan(Config['APP_KEY'],Config['BDY_SECRET_KEY'],Config['BDY_APP_NAME'])
+        if not is_need_auth():
+            _log.info("用户token尚未过期，跳过验证阶段")
+            return
+        
+        bdy = BaiDuWangPan(Config['BDY_APP_KEY'],Config['BDY_SECRET_KEY'],Config['BDY_APP_NAME'])
         res = bdy.get_device_code()
         # 出现错误
         if "errno" in res or "error_code" in res:
@@ -20,7 +38,7 @@ def auth_bdy():
             os.abort()
         
         # 显示token到控制台，让用户输入。sleep等待
-        _log.info(f"浏览器打开 {res['verification_url']} 输入验证码 {res['user_code']}")
+        _log.info(f"请使用浏览器打开 {res['verification_url']} 输入授权码 {res['user_code']}")
         _log.info("请在4分钟内完成此操作，完成后等待进程继续...")
         time.sleep(60*4)
 
@@ -34,7 +52,6 @@ def auth_bdy():
             os.abort()
         
         # 写回配置文件
-        global Config
         Config['BDY_USER_ACCESS_TOKEN'] = res["access_token"] 
         Config['BDY_USER_REFRESH_TOKEN'] = res["refresh_token"]
         Config['BDY_USER_TOKEN_OUTDATE'] = time.time() + res["expires_in"]
@@ -48,3 +65,4 @@ def auth_bdy():
 
 if __name__ == "__main__":
     _log.info(f"[start] start at {gtime.get_time_str()}")
+    auth_bdy()
